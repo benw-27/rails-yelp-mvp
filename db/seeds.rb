3e54b59143fc2265cd3545f81459dcebf76937fd
Ben Whitehouse <benw27@gmail.com>
2019-08-15 10:42:43 +0100

    completed rails restaurant mvc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "Hoxton 100", address: "Hoxton", category: "chinese", phone_number: "0678876789")
+Restaurant.create(name: "pizza hut", address: "Hoxton 123", category: "italian", phone_number: "0454546789")
+
+return "done"
